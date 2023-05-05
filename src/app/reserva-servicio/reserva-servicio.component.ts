861617f933de09e2e938b155043d5b5b06ba4ce8
faridhinostrozahm3@gmail.com <faridhinostrozahm3@gmail.com>
2023-05-05 10:41:57 -0500

    reserva servicio

---
+++ src/app/reserva-servicio/reserva-servicio.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-happytails-servicio',
+  templateUrl: './happytails-servicio.component.html',
+  styleUrls: ['./happytails-servicio.component.css']
+})
+export class HappytailsServicioComponent implements OnInit {
+  contactoForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.contactoForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      telefono: ['', [Validators.required, Validators.pattern('[0-9]+')]],
+      mensaje: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  enviarMensaje(): void {
+    if (this.contactoForm.valid) {
+      const formData = new FormData();
+      formData.append('nombre', this.contactoForm.get('nombre')!.value);
+      formData.append('correo', this.contactoForm.get('correo')!.value);
+      formData.append('telefono', this.contactoForm.get('telefono')!.value);
+      formData.append('mensaje', this.contactoForm.get('mensaje')!.value);
+
+      this.http.post('https://ejemplo.com/enviar-mensaje', formData).subscribe(
+        (response) => {
+          console.log('Mensaje enviado con éxito.');
+        },
+        (error) => {
+          console.error('Hubo un error al enviar el mensaje.');
+        }
+      );
+    } else {
+      console.error('Formulario inválido.');
+    }
+  }
+
+}
